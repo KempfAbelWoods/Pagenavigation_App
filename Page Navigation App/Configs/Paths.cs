14bd230a7e34d2a5e47f30b8086da2e046934bb7
KempfAbelWoods <131654811+KempfAbelWoods@users.noreply.github.com>
2023-05-26 22:50:34 +0200

    2ter

---
+++ Page Navigation App/Configs/Paths.cs
@@ -0,0 +1,8 @@
+﻿namespace Page_Navigation_App.Configs;
+
+public static class Paths
+{
+    public const string sqlite_path =  "/identifier.sqlite";
+
+    
+}
---
+++ Page Navigation App/DB/DbCustomer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Page_Navigation_App.Helper;
+using Page_Navigation_App.View;
+using SQLite;
+namespace Page_Navigation_App.DB;
+
+public class DbCustomer
+{
+    
+        [PrimaryKey, AutoIncrement]
+        public uint ID { get; set; }
+
+        public string Character { get; set; }
+        
+        public string BGColor { get; set; }
+
+        public string Name { get; set; }
+
+        public string Adress { get; set; }
+        
+        public string Mail { get; set; }
+        
+        public string Phone { get; set; }
+
+        
+
+
+        public static Error CreateTable(string dataSource)
+        {
+            SQLiteConnection conn = null;
+
+            try
+            {
+                conn = new SQLiteConnection(dataSource);
+                conn.CreateTable<Member>();
+
+                return null;
+            }
+            catch (Exception ex)
+            {
+                return new Error(ex.ToString());
+            }
+            finally
+            {
+                conn?.Close();
+            }
+        }
+    }
+    
