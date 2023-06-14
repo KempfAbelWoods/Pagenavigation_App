3254908841bbf40810af3e54e66484b571bfa8b5
KempfAbelWoods <131654811+KempfAbelWoods@users.noreply.github.com>
2023-06-14 14:01:07 +0200

    Fehlerbehebung, harter Exit bei Fatal Error.

--- Page Navigation App/App.xaml.cs
+++ Page Navigation App/App.xaml.cs
@@ -2,9 +2,15 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Forms;
+using Page_Navigation_App.View;
+using Page_Navigation_App.ViewModel;
+using Application = System.Windows.Application;
+using MessageBox = System.Windows.MessageBox;
 
 namespace Page_Navigation_App
 {
@@ -15,8 +21,12 @@ namespace Page_Navigation_App
     {
         private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
         {
-            MessageBox.Show("An unhandled exception just occurred: " + e.Exception.Message, "Exception Sample", MessageBoxButton.OK, MessageBoxImage.Error);
+            MessageBox.Show("An unhandled exception just occurred: " + e.Exception.Message + " You need to restart the Application", "Fatal Error",
+                MessageBoxButton.OK, MessageBoxImage.Error);
             e.Handled = true;
+            Process.Start(Process.GetCurrentProcess().MainModule.FileName);
+            //Harter Exit
+            Environment.Exit(0);
         }
     }
 }
