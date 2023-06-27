bce14fd856e1d62977e8820eaf975debe0155b86
KempfAbelWoods <131654811+KempfAbelWoods@users.noreply.github.com>
2023-06-27 21:05:01 +0200

    serialize

---
+++ Page Navigation App/Configs/SerializeMessage.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.Json;
+using Page_Navigation_App.DB;
+using Page_Navigation_App.Helper;
+
+namespace Page_Navigation_App.Configs;
+
+public class SerializeMessage
+{
+    public List<Db_Tasks> Tasks { get; set; }
+    public List<Db_Users> UsersList { get; set; }
+
+
+    public static (string , Error) SendMessage(string datasource)
+    {
+
+        var (userlist, err) = Rw_Users.Read("", Paths.sqlite_path);
+        if (err!=null) { return (null,err); }
+        var (taskList, error) = Rw_Tasks.Read("", Paths.sqlite_path);
+        if (error!=null) { return (null,error); }
+        var message = new SerializeMessage
+        {
+            Tasks = taskList,
+            UsersList = userlist
+        };
+
+        string jsonString = JsonSerializer.Serialize(message);
+
+        //TODO anstatt return json string async connection 
+        return (jsonString,null);
+
+    }
+}
+
