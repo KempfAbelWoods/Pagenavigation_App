b70bcbdd1a05c74385b582ee850ff9f30b17c680
KempfAbelWoods <131654811+KempfAbelWoods@users.noreply.github.com>
2023-06-29 19:21:26 +0200

    Connection geht fast

--- Page Navigation App/Connection/SocketServer.cs
+++ Page Navigation App/Connection/SocketServer.cs
@@ -4,6 +4,7 @@ using Page_Navigation_App.Configs;
 using Page_Navigation_App.DB;
 
 namespace Page_Navigation_App.Connection;
+
 using System;
 using System.Net;
 using System.Net.Sockets;
@@ -15,66 +16,58 @@ public class Server
 
     public async static void SocketServer()
     {
+        byte[] buffer = new byte[1024];
+        string Password = "";
+        string Username = "";
+        //string response = "";
 
-            byte[] buffer = new byte[1024];
-            string Password = "";
-            //string response = "";
-            
-            string SetIpAddress = "";
-            //IP Adresse auslesen
-            var (list, err1) = Rw_Settings.ReadwithID("3", Paths.sqlite_path);
-            if (list.Count == 1)
-            {
-                SetIpAddress = list[0].Ressource;
-            }
+        string SetIpAddress = "";
+        //IP Adresse auslesen
+        var (list, err1) = Rw_Settings.ReadwithID("3", Paths.sqlite_path);
+        if (list.Count == 1)
+        {
+            SetIpAddress = list[0].Ressource;
+        }
 
-            if (err1 != null)
-            {
-                MessageBox.Show(err1.GetException().Message);
-            }
+        if (err1 != null)
+        {
+            MessageBox.Show(err1.GetException().Message);
+        }
 
-            // IP-Adresse des Servers
-            IPAddress ipAddress = IPAddress.Parse(SetIpAddress);
+        // IP-Adresse des Servers
+        IPAddress ipAddress = IPAddress.Parse(SetIpAddress);
 
-            // Erstelle einen TCP/IP-Socket
-            TcpListener listener = new TcpListener(ipAddress, Port);
-            listener.Start();
+        // Erstelle einen TCP/IP-Socket
+        TcpListener listener = new TcpListener(ipAddress, Port);
+        listener.Start();
 
-            // Warte auf eine eingehende Verbindung
-            TcpClient client = await listener.AcceptTcpClientAsync();
+        // Warte auf eine eingehende Verbindung
+        TcpClient client = await listener.AcceptTcpClientAsync();
 
-            // Erstelle ein NetworkStream-Objekt für die Kommunikation
-            NetworkStream networkStream = client.GetStream();
+        // Erstelle ein NetworkStream-Objekt für die Kommunikation
+        NetworkStream networkStream = client.GetStream();
 
-            // Lese die Daten vom Client
-            int passwordRead = await networkStream.ReadAsync(buffer, 0, buffer.Length);
-            string dataReceived = Encoding.ASCII.GetString(buffer, 0, passwordRead);
-            
-            //Passwort auslesen
-            //TODO hier nicht Passwort von Settings lesen sondern in der DB nach User suchen und dem sein PW abfragen!!!!
-            var (list1, err2) = Rw_Settings.ReadwithID("2", Paths.sqlite_path);
+
+        // Lese die Daten vom Client
+        int ReadReceived = await networkStream.ReadAsync(buffer, 0, buffer.Length);
+        string dataReceived = Encoding.ASCII.GetString(buffer, 0, ReadReceived);
+        //Empfangene Daten auswerten
+        //TODO hier nicht Passwort von Settings lesen sondern in der DB nach User suchen und dem sein PW abfragen!!!!
+        if (dataReceived != "")
+        {
+            var (list1, err2) = Rw_Users.ReadwithUserName(dataReceived, Paths.sqlite_path);
             if (list1.Count == 1)
             {
-                Password = list1[0].Ressource;
-            }
-            if (err2 != null)
-            {
-                MessageBox.Show(err2.GetException().Message);
-            }
-            
-            if (dataReceived == Password)
-            {
-                Trace.WriteLine("Passwort korrekt!");
+                // Sende Users und Tasks an den Client
 
-                // Sende eine Antwort an den Client
-                
-                var (response, err) =  ConnectionHelper.SendMessage(Paths.sqlite_path);
+                var (response, err) = ConnectionHelper.SendMessage(Paths.sqlite_path);
                 if (err != null)
                 {
                     MessageBox.Show(err.GetException().Message);
                 }
+
                 ConnectionHelper.SendMessage(response);
-                
+
                 byte[] responseData = Encoding.ASCII.GetBytes(response);
                 await networkStream.WriteAsync(responseData, 0, responseData.Length);
 
@@ -85,7 +78,36 @@ public class Server
                 Trace.WriteLine("Verbindung geschlossen.");
                 MessageBox.Show("Verbindung geschlossen.");
             }
-        
 
+            if (err2 != null)
+            {
+                MessageBox.Show(err2.GetException().Message);
+            }
+        }
+        else
+        {
+            if (dataReceived == Paths.ConnectionCode)
+            {
+                // Sende Users und Tasks an den Client
+
+                var (response, err) = ConnectionHelper.SendMessage(Paths.sqlite_path);
+                if (err != null)
+                {
+                    MessageBox.Show(err.GetException().Message);
+                }
+
+                ConnectionHelper.SendMessage(response);
+
+                byte[] responseData = Encoding.ASCII.GetBytes(response);
+                await networkStream.WriteAsync(responseData, 0, responseData.Length);
+
+                // Schließe die Verbindung
+                client.Close();
+                listener.Stop();
+
+                Trace.WriteLine("Verbindung geschlossen.");
+                MessageBox.Show("Verbindung geschlossen.");
+            }
+        }
     }
 }
